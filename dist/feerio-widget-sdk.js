e6db626a5074e43611f6f5051d673c4b6527e032
Arian Shaikh <ariansk90@gmail.com>
2024-12-08 20:14:31 +0530

    Update version to 2.2.5 in feerio-widget-sdk.js

--- dist/feerio-widget-sdk.js
+++ dist/feerio-widget-sdk.js
@@ -17341,7 +17341,7 @@
 
 	*/
 	// for now we are approaching the 1st way
-	// VERSION ************ 2.2.4 ************
+	// VERSION ************ 2.2.5 ************
 	window.Feerio = {
 	    initializeChangelog,
 	    initializeChangelogPopup,
